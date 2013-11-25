91fd20fde484f5443a434bebcd6758759ed6a548
andrerocker <andre.souza@gmail.com>
2013-11-25 20:38:42 -0200

    build tar using system tar pahh!

--- src/workspace/export/tar.go
+++ src/workspace/export/tar.go
@@ -6,5 +6,5 @@ import (
 )
 
 func BuildTar(output io.Writer, directory string, target string) error {
-    return util.ExecuteAndWriteToStreamFromDirectory(output, directory, "tar", "--exclude", ".git", "-czvf", target, ".")
+  return util.ExecuteAndWriteToStreamFromDirectory(output, directory, "tar", "--exclude", ".git", "-czvf", target, ".")
 }
